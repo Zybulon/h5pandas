52a7182a0a52d99d852de25d2ee95b3324c1aff1
FMasson <97910143+Zybulon@users.noreply.github.com>
2023-10-11 22:34:35 +0200

    update version number

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'h5pandas'
 copyright = '2023, Frédéric MASSON'
 author = 'Frédéric MASSON'
-release = '0.1.4'
+release = '0.1.5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
