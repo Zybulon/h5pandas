7736703a1804d6e886b1169d43099ae51bf02307
FMasson <97910143+Zybulon@users.noreply.github.com>
2024-10-13 17:13:18 +0200

    change init order

--- h5pandas/__init__.py
+++ h5pandas/__init__.py
@@ -1,7 +1,6 @@
+__version__ = "0.12"
 from .h5array import HDF5ExtensionArray
 from .h5datatype import HDF5Dtype
 from .dataframe import dataset_to_dataframe, dataframe_to_hdf, ndarray_to_hdf
 from h5py import *
 from .group import Group, File
-
-__version__ = "0.12"
