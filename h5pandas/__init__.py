9e3fc43b9d2b493e10787a54c8523ca594e882c4
FMasson <97910143+Zybulon@users.noreply.github.com>
2025-11-15 19:32:08 +0100

    increment version number

--- h5pandas/__init__.py
+++ h5pandas/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.18"
+__version__ = "0.19"
 from .h5array import HDF5ExtensionArray
 from .h5datatype import HDF5Dtype
 from .dataframe import dataset_to_dataframe, dataframe_to_hdf, ndarray_to_hdf
